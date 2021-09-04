c294c16c43e397cbee6041e41ae0300bb8db7007
Tariq <tariq9853@gmail.com>
2021-09-04 23:03:39 +0200

    Deleting old files.

--- Meetkaart_Joystick/Meetkaart_Joystick_.cp
+++
@@ -1,4 +0,0 @@
-#line 1 "C:/Users/Jaspe/Documents/Meetkaart_Joystick/Meetkaart_Joystick_.c"
-void main() {
-
-}
--- Test-ok/test.bat
+++
@@ -1,2 +0,0 @@
-CD G:\Archief\M3400\M3439\M3439a\TestOK design nieuw\Firmware Manuals\Py
-py -3 hex2dec.py -H %*
